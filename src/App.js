e3626f13ce09ef792a5cccc8469f3d2d99863026
Patrick <patrickrodriguez2002@outlook.com>
2023-02-04 21:57:03 -0300

    FETCH, USE-EFFECT(localStorage) and STATES

--- src/App.js
+++ src/App.js
@@ -1,28 +1,74 @@
 import React from 'react';
-import Button from './Button';
-import Contador from './Contador';
-import Content from './Content';
+import './App.css'
+import Produto from './Produto'
 
 const App = () => {
 
-    // Hook = Open / CLose
-
-    let [Ativo, setAtivo] = React.useState(true) 
-    let [item, setItem] = React.useState(true)
-
-    function fun() {
-        setItem((item) => !item)
-        // setItem(!item)
+    const style = {
+        background: 'black',
+        margin: '5px',
+        borderRadius: '5px',
+        padding: '8px',
+        color: 'white',
+        cursor: 'pointer',
+        textAlign: 'center',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center',
     }
 
+    const display = {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center',
+    }
+
+    // ============================================
+
+    let [produto, setProduto] = React.useState(null)
+    let [item, SetItem] = React.useState(null)
+
+    // ============================================
+
+    function click({target}) {
+        setProduto(target.innerText)
+    }
+
+    React.useEffect(() => {
+        if (produto !== null) {
+            window.localStorage.setItem('produto', produto)
+        }
+    }, [produto])
+
+    React.useEffect(() => {
+        const produtoLocal = window.localStorage.getItem('produto')
+        if (produtoLocal != null) {
+            setProduto(produtoLocal)
+        }
+    })
+
+    // ============================================
 
     return (
-        <section>   
-            <button onClick={fun}>{item ? 'word' : 'palavra'}</button>
-            <Button Ativo={Ativo} setAtivo={setAtivo}/>
-            <Content Ativo={Ativo}/>
-            <Contador Ativo={Ativo} setAtivo={setAtivo}/>
-        </section>
+        <div className="App"> 
+        <header className='App-header'>
+            <h2>FETCH, USE-EFFECT(localStorage) and STATES</h2>
+                <div>
+                    <h3 style={display}>CLIQUE E ARMAZENE INFORMAÇÕES</h3>
+                    <buttton onClick={click} style={style} id='1'>Tablet</buttton>
+                    <buttton onClick={click} style={style} id='1'>Celular</buttton>
+                    { produto &&<p><b>Preferência:</b>    {produto}</p>}
+                </div>
+                <div>
+                    <h3 style={display}>CLIQUE E RECEBA & ARMAZENE INFORMAÇÕES</h3>
+                    <Produto style={display} produto={item}/>
+                </div>
+        </header>
+        </div>
     )
 
 }
--- src/Button.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Button = ({Ativo, setAtivo}) => {
-  return (<button onClick={() => setAtivo(!Ativo)}>{Ativo ? 'Abrir' : 'Fechar'}</button>)
-}
-
-export default Button
