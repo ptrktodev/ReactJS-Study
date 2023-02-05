472f4df6beceb0c94a3b22755e6690e7dbc29ae4
Patrick <patrickrodriguez2002@outlook.com>
2023-02-05 15:00:54 -0300

    sebnd comments to the list

--- src/App.js
+++ src/App.js
@@ -1,73 +1,80 @@
 import React from 'react';
 import './App.css'
-import Produto from './Produto'
 
 const App = () => {
 
-    const style = {
-        background: 'black',
-        margin: '5px',
-        borderRadius: '5px',
-        padding: '8px',
-        color: 'white',
+    const styleInput = {
         cursor: 'pointer',
-        textAlign: 'center',
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        textAlign: 'center',
-    }
-
-    const display = {
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        textAlign: 'center',
+        padding: '10px',
+        marginRight: '10px',
+        borderRadius: '5px'
     }
 
     // ============================================
+    const [coment, setComent] = React.useState([])
+    const [input, setinput] = React.useState('')
+    const [msg, setMsg] = React.useState('')
+    const focusInput = React.useRef()
+    const [ativo, setAtivo] = React.useState(false)
 
-    let [produto, setProduto] = React.useState(null)
-    let [item, SetItem] = React.useState(null)
-
-    // ============================================
-
-    function click({target}) {
-        setProduto(target.innerText)
+    function send() {
+        if (input) {
+            setComent([...coment, input])
+            setinput('')
+            focusInput.current.focus()
+            setAtivo(!ativo)
+        } else {
+            let message = 'Adicone um valor'
+            setMsg(message)
+            setAtivo(!ativo)
+        }
     }
 
-    React.useEffect(() => {
-        if (produto !== null) {
-            window.localStorage.setItem('produto', produto)
-        }
-    }, [produto])
+    function changeValue({target}) {
+        setinput(target.value)
+    }
 
-    React.useEffect(() => {
-        const produtoLocal = window.localStorage.getItem('produto')
-        if (produtoLocal != null) {
-            setProduto(produtoLocal)
+    function resetList(){
+        setComent([])
+    }
+
+    function lastItem() {
+        coment.pop()
+        setComent([...coment])
+    }
+
+    function keyTecla(event) {
+        if (event.key === 'Enter') {
+            if (input) {
+                setComent([...coment, input])
+                setinput('')
+                focusInput.current.focus()
+                setAtivo(!ativo)
+            } else {
+                let message = 'Adicone um valor'
+                setMsg(message)
+                setAtivo(!ativo)
+            }
         }
-    })
+    }
 
     // ============================================
 
     return (
         <div className="App"> 
-        <header className='App-header'>
-            <h2>FETCH, USE-EFFECT(localStorage) and STATES</h2>
-                <div>
-                    <h3 style={display}>CLIQUE E ARMAZENE INFORMAÇÕES</h3>
-                    <buttton onClick={click} style={style} id='1'>Tablet</buttton>
-                    <buttton onClick={click} style={style} id='1'>Celular</buttton>
-                    { produto &&<p><b>Preferência:</b>    {produto}</p>}
-                </div>
-                <div>
-                    <h3 style={display}>CLIQUE E RECEBA & ARMAZENE INFORMAÇÕES</h3>
-                    <Produto style={display} produto={item}/>
-                </div>
-        </header>
+            <header className='App-header'>
+                <h1>SEND COMMENTS TO THE LIST</h1>
+                <ul>
+                    {coment.map((el) =>
+                        <li key={el}>{el}</li>
+                    )}
+                </ul>
+                <input ref={focusInput} onKeyDown={keyTecla} style={styleInput} type='text' value={input} onChange={changeValue} />
+                <button style={styleInput} onClick={send}>Send</button> 
+                <button style={styleInput} onClick={lastItem}>Clear last item</button>
+                <button style={styleInput} onClick={resetList}>Reset list</button> 
+                {msg && !ativo && <p>{msg}</p>}
+            </header>
         </div>
     )
 
